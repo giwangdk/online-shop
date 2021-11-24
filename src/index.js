da7b0d76e0a115c0f769fc46bbda304cbc7a7f6a
gidwikintan <gidwikintan@gmail.com>
2021-11-25 06:07:22 +0700

    config store

--- src/index.js
+++ src/index.js
@@ -2,9 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+
+import productsReducer from './featues/productsSlice';
+
+const store = configureStore({
+  reducer: {
+    products: productsReducer,
+  }
+})
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
