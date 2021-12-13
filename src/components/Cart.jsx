c6e5c46aed567c49bc2e0f5be96f06dac42cb6ce
gidwikintan <gidwikintan@gmail.com>
2021-12-14 06:35:56 +0700

    create remove from cart reducer

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,8 +1,13 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import {Link} from 'react-router-dom'
-
+import { removeFromCart } from '../features/cartSlice';
 const Cart = () => {
-    const cart  = useSelector(state=>state.cart)
+    const cart = useSelector(state => state.cart)
+    const dispatch = useDispatch()
+
+    const handleRemoveFromCart = (cartItem)=>{
+        dispatch(removeFromCart(cartItem))
+    }
     return (
         <div className="cart-container">
             <h2>Shopping Cart</h2>
@@ -33,7 +38,7 @@ const Cart = () => {
                                     <div>
                                         <h3>{cartItem.name}</h3>
                                         <p>{cartItem.desc}</p>
-                                        <button>remove</button>
+                                        <button onClick={()=>handleRemoveFromCart(cartItem)}>remove</button>
                                     </div>
                                 </div>
                                 <div className="cart-product-price">${cartItem.price}</div>
